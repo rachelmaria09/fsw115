9e9d1422e8d20dd66ef293c6d7fa8b6ce6a53571
rachelmaria09 <rmgildea@gmail.com>
2020-08-08 11:35:05 -0400

    update1

---
+++ week2/project1/project1.js
@@ -0,0 +1,48 @@
+var weather = [
+    {
+        "weather": [
+        {
+        "id": 300,
+        "main": "Drizzle",
+        "description": "light intensity drizzle",
+        "icon": "09d"
+        }
+        ],
+        "base": "stations",
+        "main": {
+        "temp": 280.32,
+        "pressure": 1012,
+        "humidity": 81,
+        "temp_min": 279.15,
+        "temp_max": 281.15
+        },
+        "visibility": 10000,
+        "wind": {
+        "speed": 4.1,
+        "deg": 80
+        },
+        "clouds": {
+        "all": 90
+        },
+        "dt": 1485789600,
+        "sys": {
+        "type": 1,
+        "id": 5091,
+        "message": 0.0103,
+        "country": "GB",
+        "sunrise": 1485762037,
+        "sunset": 1485794875
+        },
+        "id": 2643743,
+        "name": "London",
+        "cod": 200
+        }
+]
+
+var weatherList = document.getElementById("list")
+
+for (var i = 0; i < weather.length; i++) {
+    var newWeather = document.createElement("li")
+    newWeather.textContent = weather[i]
+    weatherList.append(newWeather)
+}
