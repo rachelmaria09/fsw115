aeb926b783398028611b899a7d37e6441cb32262
rachelmaria09 <rmgildea@gmail.com>
2020-08-08 14:53:19 -0400

    final

--- week2/project1/project1.js
+++ week2/project1/project1.js
@@ -1,38 +1,26 @@
 var weather = [
     {
-        "weather": [
-        {
         "id": 300,
         "main": "Drizzle",
         "description": "light intensity drizzle",
-        "icon": "09d"
-        }
-        ],
+        "icon": "09d",
         "base": "stations",
-        "main": {
         "temp": 280.32,
         "pressure": 1012,
         "humidity": 81,
         "temp_min": 279.15,
-        "temp_max": 281.15
-        },
+        "temp_max": 281.15,
         "visibility": 10000,
-        "wind": {
         "speed": 4.1,
-        "deg": 80
-        },
-        "clouds": {
-        "all": 90
-        },
+        "deg": 80,
+        "all": 90,
         "dt": 1485789600,
-        "sys": {
         "type": 1,
         "id": 5091,
         "message": 0.0103,
         "country": "GB",
         "sunrise": 1485762037,
-        "sunset": 1485794875
-        },
+        "sunset": 1485794875,
         "id": 2643743,
         "name": "London",
         "cod": 200
@@ -42,7 +30,11 @@ var weather = [
 var weatherList = document.getElementById("list")
 
 for (var i = 0; i < weather.length; i++) {
-    var newWeather = document.createElement("li")
-    newWeather.textContent = weather[i]
-    weatherList.append(newWeather)
+    const obj = weather[i]
+        for (var x = 0; x < Object.keys(obj).length; x++) {
+        console.log(Object.keys(obj)[x])
+        var newWeather = document.createElement("li")
+        newWeather.textContent = Object.keys(obj)[x] + ": " + Object.values(obj)[x]
+        weatherList.append(newWeather)
+    }
 }
