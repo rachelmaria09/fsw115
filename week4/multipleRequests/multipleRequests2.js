927d6857c5087378b4aed5acd03145ffd9a1f05e
rachelmaria09 <rmgildea@gmail.com>
2020-08-23 17:46:57 -0400

    second

--- week4/multipleRequests/multipleRequests2.js
+++ week4/multipleRequests/multipleRequests2.js
@@ -3,9 +3,10 @@ var btn2 = document.getElementById("btn2")
 function btnClick(){
     axios.get("https://rickandmortyapi.com/api/episode")
     .then(response => {
-        for(let i = 0; i < response.data.length; i++) {
+        for(let i = 0; i < response.data.results; i++) {
             const episodes = document.createElement('div')
-            document.body.append(div)
+            episodes.textContent = response.data.results[i]
+            document.body.append(episodes)[i].name
         }
      })
      .catch(error => console.error(error))
