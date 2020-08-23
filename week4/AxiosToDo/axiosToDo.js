ff689aea428ba6edc638c24d8133e72a38531f32
rachelmaria09 <rmgildea@gmail.com>
2020-08-23 18:20:00 -0400

    second

--- week4/AxiosToDo/axiosToDo.js
+++ week4/AxiosToDo/axiosToDo.js
@@ -1,19 +1,10 @@
-//Title
-document.head = "ToDo List"
-
-//GET
 axios.get("https://api.vschool.io/rachelgildea/todo")
-    .then(response => {
-        for(let i = 0; i < response.data.length; i++) {
-            const todoList = document.createElement('div')
-            todoList.textContent = response.data[i].title
-            document.body.appendChild(todoList)
-        }
-    })
-    .catch(error => console.log(error))
-
-// //POST
-// axios.post(https://api.vschool.io/rachelgildea/todo, {
-//     name: "Rachel",
-    
-// })
+.then(response => {
+    console.log(response.data)
+    for(let i = 0; i < response.data.length; i++) {
+        const episodes = document.createElement('div')
+        episodes.textContent = response.data[i].title
+        document.body.append(episodes)
+    }
+ })
+ .catch(error => console.error(error))
