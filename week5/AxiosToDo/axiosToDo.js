3524853d5dfd9bc8b7c8f894df33c82526565379
rachelmaria09 <rmgildea@gmail.com>
2020-09-04 14:21:05 -0400

    final

--- week5/AxiosToDo/axiosToDo.js
+++ week5/AxiosToDo/axiosToDo.js
@@ -5,20 +5,42 @@ axios.get("https://api.vschool.io/rachelgildea/todo")
     for(let i = 0; i < response.data.length; i++) {
         const todos = document.createElement('div')
         todos.textContent = response.data[i].title
+        var id = response.data[i]._id
+        if(response.data[i].completed) {checkbox.checked = true}
+        const checkbox = document.createElement('input')
+        checkbox.type="checkbox"
+        checkbox.addEventListener("click", function(event) {
+            if(checkbox.checked === true) {
+                todos.style.textDecoration = "line-through"
+            } if(checkbox.checked === false) {
+                todos.style.textDecoration = "none"
+            }
+        axios.put(`https://api.vschool.io/rachelgildea/todo/${id}`, {completed: checkbox.checked})
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+        })
+
+        const deleteBtn = document.createElement('button')
+        todos.append(deleteBtn)
+        deleteBtn.textContent = "X"
+        deleteBtn.addEventListener("click", function(event) {
+            //delete
+            axios.delete(`https://api.vschool.io/rachelgildea/todo/${id}`)
+            .then(response => console.log(response))
+            .catch(error=> console.log(error))
+        })
+
+        todos.appendChild(checkbox)
         document.body.append(todos)
-                if(response.data[i].completed !== true) {
-                    todos.style.textDecoration = "line-through"
-                }
-        }
-        let checkbox = document.createElement('input')
-            checkbox.type="checkbox"
-            todos.appendChild(checkbox)
- })
+            if(response.data === true) {
+                todos.style.textDecoration = "line-through"
+            }
+    }
+})
  .catch(error => console.error(error))
 
  //post
 const todoForm = document.todoForm
-
 todoForm.addEventListener("submit", function(event){
     event.preventDefault()
 
@@ -27,13 +49,11 @@ todoForm.addEventListener("submit", function(event){
         description: todoForm.description.value,
         price: todoForm.price.value
     }
+    console.log(newTodo)
 
     axios.post("https://api.vschool.io/rachelgildea/todo", newTodo)
     .then(response => console.log(response.data))
     .catch(error => console.log(error))
 })
 
-//put
-axios.put("https://api.vschool.io/rachelgildea/todo/<id>", {})
-    .then(response => console.log(response.data))
-    .catch(error => console.log(error))
+
